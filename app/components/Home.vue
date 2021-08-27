64be05c41af28007cf4a19a326a4029be6fe1077
Igor Randjelovic <rigor789@gmail.com>
2021-08-27 15:53:52 +0200

    chore: init

---
+++ app/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Home"/>
+    </ActionBar>
+
+    <GridLayout>
+      <Label class="info">
+        <FormattedString>
+          <Span class="fas" text.decode="&#xf135; "/>
+          <Span :text="message"/>
+        </FormattedString>
+      </Label>
+    </GridLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+  import Vue from "nativescript-vue";
+
+  export default Vue.extend({
+    computed: {
+      message() {
+        return "Blank {N}-Vue app";
+      }
+    }
+  });
+</script>
+
+<style scoped lang="scss">
+  .info {
+    font-size: 20;
+    horizontal-align: center;
+    vertical-align: center;
+  }
+</style>
